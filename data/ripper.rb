b1e0fe82fbbbb23abbc5b94101e99b1625cf5016
Robert Brook <mail@robertbrook.com>
2009-11-09 17:44:03 +0000

    big push to functions

--- data/ripper.rb
+++ data/ripper.rb
@@ -1,11 +1,9 @@
 require 'rubygems'
-require 'activerecord'
 require 'nokogiri'
-
-
 
 doc = Nokogiri::XML(open('1996-1997.xml'))
 
 doc.xpath('//motion').each do |motion|
-    p motion.id
+    p motion.xpath("number/text()")
 end
+
