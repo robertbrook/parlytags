0740bcd7431e8280620f05b3d1dcfa9115e7c91d
lizconlan <Liz@liz-conlans-macbook.local>
2009-11-12 11:59:21 +0000

    added EDM number and sponsor to the view

--- app/models/proposer.rb
+++ app/models/proposer.rb
@@ -1,3 +1,16 @@
 class Proposer < ActiveRecord::Base
   belongs_to :edm
+  
+  def display_name
+    first_name = ""
+    surname = ""
+    if name[/, (.*)/]
+      first_name = $1
+    end
+    if name[/^(.*),/]
+      surname = $1
+    end
+    "#{first_name} #{surname}"
+  end
+    
 end
