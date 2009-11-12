7cf29cdfaef818eebae85bac1b6e9990cf828379
lizconlan <Liz@liz-conlans-macbook.local>
2009-11-12 23:14:20 +0000

    fidgeting with the way the number of signatories is counted and displayed

--- app/models/edm.rb
+++ app/models/edm.rb
@@ -3,4 +3,12 @@ class Edm < ActiveRecord::Base
   has_many :proposers, :through => :member_signatures, :source => :signature, :source_type => 'Proposer'
   has_many :seconders, :through => :member_signatures, :source => :signature, :source_type => 'Seconder'
   has_many :signatories, :through => :member_signatures, :source => :signature, :source_type => 'Signatory'
+  
+  def signatories_and_seconders_count
+    signatories.count + seconders.count
+  end
+  
+  def seconders_count
+    seconders.count
+  end
 end
