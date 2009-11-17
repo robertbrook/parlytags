04955c9591dd4ebbc2e63537e7aeced8be7a31b2
lizconlan <Liz@liz-conlans-macbook.local>
2009-11-17 17:13:05 +0000

    minor tweak to url structure; page for alphabetical list of members

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,4 +1,8 @@
 class MembersController < ApplicationController
+  
+  def index
+    @members = Member.paginate :page => params[:page], :order => 'name'
+  end
     
   def show
     member_slug = params[:member]
