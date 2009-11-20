1695c746a642ec3c108621c314d6eb3f45f1039a
lizconlan <Liz@liz-conlans-macbook.local>
2009-11-20 21:41:44 +0000

    having another go at that last

--- db/migrate/20091120120336_change_signatures_date_to_datetime.rb
+++ db/migrate/20091120120336_change_signatures_date_to_datetime.rb
@@ -1,11 +1,11 @@
 class ChangeSignaturesDateToDatetime < ActiveRecord::Migration
   def self.up
-    remove_column :signatures, :datetime
+    remove_column :signatures, :date
     add_column :signatures, :date, :date
   end
 
   def self.down
-    remove_column :signatures, :datetime
+    remove_column :signatures, :date
     add_column :signatures, :date, :string
   end
 end
