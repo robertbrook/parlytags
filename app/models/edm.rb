83c13c53c92b3db13710fa3e888bb4d7c16ee2c7
lizconlan <Liz@liz-conlans-macbook.local>
2009-11-20 12:40:16 +0000

    slightly less awful date format

--- app/models/edm.rb
+++ app/models/edm.rb
@@ -4,6 +4,9 @@ class Edm < ActiveRecord::Base
   has_many :seconders, :through => :motion_signatures, :source => :signature, :conditions => "type = 'Seconder'"
   has_many :signatories, :through => :motion_signatures, :source => :signature, :conditions => "type = 'Signatory'"
   belongs_to :session
+  has_many :edm_amendments
+  
+  delegate :name, :to => :session, :prefix => :session
   
   def signatories_and_seconders_count
     signatories.size + seconders.size
@@ -12,4 +15,30 @@ class Edm < ActiveRecord::Base
   def seconders_count
     seconders.size
   end
+  
+  def has_proposer?
+    proposers.size > 0
+  end
+  
+  def is_amendment?
+    amendment_format = /\d+A\d+/
+    if number =~ amendment_format
+      true
+    else
+      false
+    end
+  end
+  
+  def has_amendments?
+    edm_amendments.size > 0
+  end
+  
+  def edm_amended
+    amendment_format = /(\d+)A\d+/
+    if number =~ amendment_format
+      return Edm.find_by_number_and_session_id($1, session_id)
+    end
+    nil
+  end
+  
 end
