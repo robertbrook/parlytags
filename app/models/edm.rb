3438d16d9317c73f1b49fa6a134d7ec3aac03eea
lizconlan <Liz@liz-conlans-macbook.local>
2010-01-28 14:46:13 +0000

    geotag generation and tagged item retrieval for edms

--- app/models/edm.rb
+++ app/models/edm.rb
@@ -9,12 +9,57 @@ class Edm < ActiveRecord::Base
   delegate :name, :to => :session, :prefix => :session
   
   acts_as_tree :order => "amendment_number"
-  is_taggable :tags
+  is_taggable :tags, :geotags
   
   class << self
     def amendment_format
       /(\d+)A(\d+)/
     end
+    
+    def find_all_by_tag tag_name, tag_kind="tag"
+      if tag_name.is_a?String
+        tag = Tag.find_by_name_and_kind(tag_name, tag_kind)
+        if tag
+          tag_links = tag.taggings.find_all_by_taggable_type(self.name)
+          if tag_links
+            instance_ids = tag_links.collect { |x| x.taggable_id }
+            return self.find(instance_ids)
+          end
+        end
+      elsif tag_name.is_a?Array
+        tag_list = %Q|'#{tag_name.join("','")}'|
+        tags = Tag.find(:all, :conditions => "name in (#{tag_list}) AND kind='#{tag_kind}'")
+        if tags
+          instance_ids = []
+          tags.each do |tag|
+            tag_links = tag.taggings.find_all_by_taggable_type(self.name)
+            if tag_links
+              tag_ids = tag_links.collect { |x| x.taggable_id }
+              instance_ids << tag_ids
+            end
+          end
+          return self.find(instance_ids)
+        end
+      end
+    end
+  end
+  
+  def generate_geotags
+    geotags = ""
+    tag_list.each do |tag|
+      places = Place.find_all_by_ascii_name(tag)
+      place_list = places.collect { |x| "#{x.id}" }
+      unless place_list.blank?
+        if geotags.blank?
+          geotags = place_list.join(',')
+        else
+          geotags = "#{geotags}, #{place_list.join(',')}"
+        end
+      end
+    end
+    self.geotag_list = geotags
+    save!
+    self.geotag_list
   end
   
   def signatories_and_seconders_count
