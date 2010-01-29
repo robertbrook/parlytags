f8c6e25fd3747df6e059fa801f81488ba76a3dc4
lizconlan <Liz@liz-conlans-macbook.local>
2010-01-29 21:06:58 +0000

    bug fix for Place.find_all_by_ascii_name_or_alternate_names

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,8 +2,8 @@ class Place < ActiveRecord::Base
   
   class << self
     def find_all_by_ascii_name_or_alternate_names(term)
-      term = term.gsub("'","\\'")
-      find(:all, :conditions => "ascii_name = '#{term}' or alternate_names='#{term}'")
+      term = term.gsub("'","\\'").strip
+      find(:all, :conditions => "ascii_name = \'#{term}\' or alternate_names=\'#{term}\'")
     end
   end
   
