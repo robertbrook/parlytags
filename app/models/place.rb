fc5bedd1deb17d772a2e88c0a3f7521e678d7fd3
lizconlan <Liz@liz-conlans-macbook.local>
2010-02-01 17:53:32 +0000

    improved find_all_by_ascii_name_or_alternate_names method

--- app/models/place.rb
+++ app/models/place.rb
@@ -4,8 +4,12 @@ class Place < ActiveRecord::Base
     def find_all_by_ascii_name_or_alternate_names(term)
       term = term.gsub("'","\\'").strip
       places = find_all_by_ascii_name(term)
-      if places.blank?
-       places = find(:all, :conditions => "alternate_names like \'%#{term}%\'")
+      other_places = find(
+        :all, 
+        :conditions => "alternate_names = \'#{term}\' or alternate_names like \'#{term},%\' or alternate_names like \'%,#{term},%\' or alternate_names like \'%,#{term}\'"
+        )
+      other_places.each do |place|
+        places << place unless places.include?(place)
       end
       places
     end
