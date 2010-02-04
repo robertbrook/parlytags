8912edab5ecd8b12b9115bb04dc8b4d867b38e4b
Robert Brook <mail@robertbrook.com>
2010-02-04 12:49:55 +0000

    merge

--- lib/data_loader.rb
+++ lib/data_loader.rb
@@ -4,10 +4,8 @@ module ParlyTags; end
 module ParlyTags::DataLoader
 
   DATA_DIR = File.expand_path(File.dirname(__FILE__) + '/../data')
-  
-  SAMPLE_FILES = ["#{DATA_DIR}/1996-1997.xml", "#{DATA_DIR}/2000-2001.xml"]
-  #SAMPLE_FILES = ["#{DATA_DIR}/test.xml"]
-  
+  # EDMS_FILES = ["#{DATA_DIR}/1996-1997.xml", "#{DATA_DIR}/2000-2001.xml"]  
+  EDMS_FILES = ["#{DATA_DIR}/test.xml"]  
   GEO_FILE = "#{DATA_DIR}/GB.txt"
 
   def load_places
@@ -41,12 +39,15 @@ module ParlyTags::DataLoader
   end
   
   def load_edms
+    
+    log = Logger.new(STDOUT)
+    
     Edm.delete_all
     Member.delete_all
     Signature.delete_all
     Session.delete_all
     
-    SAMPLE_FILES.each do |file|
+    EDMS_FILES.each do |file|
       
       # TODO: check file actually exists!
       doc = Nokogiri::XML(open(file))
@@ -59,7 +60,7 @@ module ParlyTags::DataLoader
         session = Session.find_or_create_by_name(motion.xpath("session/text()").to_s)
         
         # create an Edm
-        puts "loading #{motion.xpath("number/text()").to_s}"  
+        log << "\n#{motion.xpath("number/text()").to_s} "  
         
         edm_text = motion.xpath("text/text()").to_s
         
@@ -99,39 +100,42 @@ module ParlyTags::DataLoader
           signature_type = signature.xpath("type/text()").to_s
         
           # make an appropriate sub-type of Signature and attach it to the Edm and the Session
-          puts " creating signature (#{signature_type})"
           case signature_type
             when 'Proposed'
               new_signature = Proposer.new :date => signature_date, :member_id => member.id, :edm_id => edm.id, :session_id => session.id
               edm.proposers << new_signature
               session.proposers << new_signature
+              log << 'p'
             when 'Seconded'
               new_signature = Seconder.new :date => signature_date, :member_id => member.id, :edm_id => edm.id, :session_id => session.id
               edm.seconders << new_signature
               session.seconders << new_signature
+              log << '2nd'
             when 'Signed'
               new_signature = Signatory.new :date => signature_date, :member_id => member.id, :edm_id => edm.id, :session_id => session.id
               edm.signatories << new_signature
               session.signatories << new_signature
+              log << 's'
             else
               raise "Unrecognised signature type: #{signature_type}"
           end
         end
       end
     
-      puts "wiring up the amendments"
       amendments.each do |amendment|
         parts = amendment.number.split("A")
+        log << parts
         amendment_number = parts.pop()
         amended_edm = parts.join("A")
         parent = Edm.find_by_number_and_session_id(amended_edm, amendment.session_id)
+        
         if parent
           amendment.amendment_number = amendment_number
           amendment.parent_id = parent.id
           amendment.save!
         end
+        log << "\n"
       end
-      puts ""
     end  
   end
   
