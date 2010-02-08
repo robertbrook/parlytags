2a546f93dea20a8594ace9046838e22b2b017fd8
lizconlan <Liz@liz-conlans-macbook.local>
2010-02-08 15:23:55 +0000

    search results presented as list of 10 nearest items where a place is found; reverts to paginated search where a non-place term is used

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,16 +3,10 @@ class SearchController < ApplicationController
   def index
     term = params[:q]
     if term
-      results = do_search(term.strip)
-      if results
-        items = Item.find_all_by_tag(results.name)
-        @results = items.paginate :page => params[:page]
-      end
-      @place = Place.find_all_by_ascii_name_or_alternate_names(term)
+      do_search(term.strip)
       if @place.blank?
         @place = nil
       else
-        @place = @place.first
         @map = GMap.new("map")
         @map.control_init(:large_map => true,:map_type => false)
         @map.center_zoom_init([@place.lat, @place.lng], 14)
@@ -24,18 +18,20 @@ class SearchController < ApplicationController
     def do_search term
       @last_search_term = term
       @searched_for = term
-      tags = Tag.find_by_name(term)
-      unless tags
-        places = Place.find_all_by_ascii_name_or_alternate_names(term)
-        unless places.empty?
-          term = places.first.ascii_name
-          tags = Tag.find_by_name(term)
-          if tags
-            @usually_known_as = term
-          end
+      places = Place.find_all_by_ascii_name_or_alternate_names(term)
+      unless places.empty?
+        @place = places.first
+        if @place.ascii_name != term
+          @usually_known_as = @place.ascii_name
+        end
+        @results = @place.find_nearby_items(10)
+      else
+        tag = Tag.find_by_name(term)
+        if tag
+          items = Item.find_all_by_tag(tag.name)
+          @results = items.paginate :page => params[:page]
         end
       end
-      tags
     end
   
 end
